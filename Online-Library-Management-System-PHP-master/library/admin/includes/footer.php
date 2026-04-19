0c391197743aa1af4f7c1d05c5d058f4e8c17f12
Piyush Tiwari <www.piyush789@gmail.com>
2026-04-19 16:31:35 +0530

    initial commit - add project files

---
+++ Online-Library-Management-System-PHP-master/library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2026 Online Library Management System |<a href="https://www.linkedin.com/in/piyush-tiwari-b94669287?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank"  > Designed by : Piyush Tiwari</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
